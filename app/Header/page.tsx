b3f82a81c24933757c6fe61db5b6b90cb8160fc4
HaiderAli77 <r.haiderali222@gmail.com>
2024-12-08 12:34:29 +0500

    Remove Error

--- app/Header/page.tsx
+++ app/Header/page.tsx
@@ -9,9 +9,6 @@ const Header = () => {
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
-  <head>
-    <meta name="google-site-verification" content="4zq3kiTLUKsY9zGoZ44txOmTpZqYYcd4TtbUZ--iXno" />
-  </head>
   return (
     <header className="bg-white py-6 shadow-md">
       <div className="max-w-7xl mx-auto flex justify-between items-center px-6">
