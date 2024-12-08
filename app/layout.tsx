bacd981fc3035337156a7e8cff1f3f84c8a42e4d
HaiderAli77 <r.haiderali222@gmail.com>
2024-12-08 12:55:59 +0500

    Remove Error

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-<head>
-  <meta name="google-site-verification" content="4zq3kiTLUKsY9zGoZ44txOmTpZqYYcd4TtbUZ--iXno" />
-</head>
+
 export const metadata: Metadata = {
   title: "NextSig",
   description: "Email Signature Generator",
