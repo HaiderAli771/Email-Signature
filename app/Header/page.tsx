c7872e278708a4209114f10ac1d2f325012ef0c2
HaiderAli77 <r.haiderali222@gmail.com>
2024-12-08 14:08:52 +0500

    Remove Error

--- app/Header/page.tsx
+++ app/Header/page.tsx
@@ -10,11 +10,6 @@ const Header = () => {
     setIsOpen(!isOpen);
 
   };
-  <html>
-    <head>
-      <meta name="google-site-verification" content="4zq3kiTLUKsY9zGoZ44txOmTpZqYYcd4TtbUZ--iXno" />
-    </head>
-  </html>
   return (
     <header className="bg-white py-6 shadow-md">
       <div className="max-w-7xl mx-auto flex justify-between items-center px-6">
