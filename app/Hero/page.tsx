4474d6f585abe069b8ae0e7e1a2b398168ec01ec
HaiderAli77 <r.haiderali222@gmail.com>
2024-12-11 15:32:14 +0500

    Remove Error

--- app/Hero/page.tsx
+++ app/Hero/page.tsx
@@ -6,12 +6,9 @@ const page = () => {
     <>
       <section className="flex flex-col md:flex-row items-center md:pl-2 md:pr-2 justify-center h-screen bg-blue-600 text-white">
         <div className="relative text-center md:text-left max-w-lg md:mr-10 px-6 md:px-0">
-          <h1 className="text-5xl md:text-6xl font-bold mb-6 leading-tight">
-            Effortless <span className="text-yellow-400">Digital Signatures</span>
+          <h1 className="text-3xl md:text-5xl font-semibold mb-6 bg-gradient-to-r from-white via-cyan-300 to-teal-400 bg-clip-text text-transparent leading-snug drop-shadow-lg">
+            Create Stunning Email Signatures That Impress
           </h1>
-          <p className="text-xl mb-8 opacity-90">
-            Secure, fast, and easy-to-use signature solutions for modern businesses.
-          </p>
           <Link href='./Editor' className="px-8 py-4 bg-yellow-400 text-gray-900 font-semibold rounded-md shadow-lg hover:bg-yellow-500 transition">
             Create Your Signature
           </Link>
