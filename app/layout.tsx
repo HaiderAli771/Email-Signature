2583c8268ff6b048cf70cee584d81e9fc65ecde0
HaiderAli77 <r.haiderali222@gmail.com>
2024-12-12 11:52:26 +0500

    Remove Error

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Email Signature Generator",
-  description: "Create professional and eye-catching email signatures with ease using our Email Signature Generator. Designed for individuals and businesses alike, our platform helps you craft stunning signatures that elevate your email communication. Whether you’re looking to enhance your personal branding or establish a strong corporate identity, our tool provides the perfect solution.",
+  description: "Create professional and eye-catching email signatures with ease using our Email Signature Generator. It is Easy to use with more than 10 templates. It is free.",
 };
 
 export default function RootLayout({
